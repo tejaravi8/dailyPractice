40c643d736e732f0cc091a21530ceab4aeb2bc2b
tejaravi8 <botsaraviteja@gmail.com>
2026-04-07 15:13:11 +0530

    practice

---
+++ interview/prep/recollect.py
@@ -0,0 +1,26 @@
+# abc=input("eneter something: ").split()
+# while True:
+#     arr_values=list(input("enter: ").split())
+#     print(arr_values)
+
+# abc=list(map(int,input().split()))
+# print(abc)
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+# print("-----------------")
+# # print(arr)
+# for i in arr:
+#     print(type(i),i)
+# print("-----------------")
+
+# freq={'banana': 3, 'pple': 2, 'range': 1}
+
+# lowest=min(freq.values())
+# high=max(freq.values())
+
+# low=[i for i,j in freq.items() if j==lowest]
+# high=[i for i,j in freq.items() if j==high]
+# print(min(low),"  ",max(high))
+# print(min(freq.keys()))
+
